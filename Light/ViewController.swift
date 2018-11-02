6bbb9652b8ca7dd4f3e21492a4de074c7bb7d353
Sterre <sterresmit98@gmail.com>
2018-11-02 13:57:44 +0100

    Hierbij lever ik de afgemaakte app in en readme toegevoegd.

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var lightOn = true
 
+    @IBOutlet weak var lightButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+@IBAction func buttonPressed(_ sender: Any) {
+    lightOn = !lightOn
+    updateUI()
+            }
+func updateUI() {
+view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    
+
+        
+    }
 
 
-}
+
+
 
---
+++ README.md
@@ -0,0 +1,2 @@
+// LIGHT APP
+De functionaliteit van deze app houdt in dat wanneer het scherm aangetikt wordt de achtergrond verandert van kleur, van wit naar zwart en weer terug. 
